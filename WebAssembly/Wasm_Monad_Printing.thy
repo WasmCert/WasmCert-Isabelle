ddf07d10c2372789f7a72444ebafb73acee6b481
Conrad <cnrdwtt@gmail.com>
2021-09-01 15:23:31 +0100

    fix exported function

--- WebAssembly/Wasm_Monad_Printing.thy
+++ WebAssembly/Wasm_Monad_Printing.thy
@@ -10,7 +10,7 @@ export_code open nat_of_byte byte_of_nat
                  isabelle_int64_to_ocaml_int64
                  ocaml_char_to_isabelle_byte
                  isabelle_byte_to_ocaml_char
-                 m_imports module_type_checker interp_instantiate_m typing run_m run_invoke_m
+                 m_imports make_empty_store_m module_type_checker interp_instantiate_m typing run_m run_invoke_m
   in OCaml_imp module_name WasmRef_Isa_m file_prefix WasmRef_Isa_m
 
 end
